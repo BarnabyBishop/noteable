d74f622ddd69e7c6f59b74de7fdeff01860c6391
Barnaby Bishop <barneyb@gmail.com>
2014-09-28 19:26:31 +1000

    Inital commit. Created project structure, added grunt and grunt tasks.

---
+++ app.js
@@ -0,0 +1,77 @@
+(function() {
+  var app, bodyParser, cookieParser, debug, express, favicon, logger, path, routes, server, users;
+
+  debug = require('debug')('noteable');
+
+  express = require('express');
+
+  path = require('path');
+
+  favicon = require('serve-favicon');
+
+  logger = require('morgan');
+
+  cookieParser = require('cookie-parser');
+
+  bodyParser = require('body-parser');
+
+  routes = require('./routes/index');
+
+  users = require('./routes/users');
+
+  app = express();
+
+  app.set('views', path.join(__dirname, 'views'));
+
+  app.set('view engine', 'jade');
+
+  app.use(logger('dev'));
+
+  app.use(bodyParser.json());
+
+  app.use(bodyParser.urlencoded({
+    extended: false
+  }));
+
+  app.use(cookieParser());
+
+  app.use(express["static"](path.join(__dirname, 'public')));
+
+  app.use('/', routes);
+
+  app.use('/users', users);
+
+  app.use(function(req, res, next) {
+    var err;
+    err = new Error('Not Found');
+    err.status = 404;
+    return next(err);
+  });
+
+  if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+      res.status(err.status || 500);
+      return res.render('error', {
+        message: err.message,
+        error: err
+      });
+    });
+  }
+
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    return res.render('error', {
+      message: err.message,
+      error: {}
+    });
+  });
+
+  module.exports = app;
+
+  app.set('port', process.env.PORT || 3000);
+
+  server = app.listen(app.get('port'), function() {
+    return debug('Express server listening on port ' + server.address().port);
+  });
+
+}).call(this);
